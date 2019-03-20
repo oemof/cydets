152bc0b8d9bb3f67b4c2e6cd2e9be281ca27d35a
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2019-03-20 15:12:05 +0100

    Add example TES series

--- main.m
+++ main.m
@@ -3,6 +3,13 @@
 profile = csvread('cav_level.csv');
 profile = transpose(profile);
 
+% Ungetestet: normieren der Zeitreihe
+% Siehe:
+% https://www.mathworks.com/matlabcentral/answers/
+% 196798-how-to-normalize-values-in-a-matrix-to-be-between-0-and-1
+profile = profile - min(profile(:))
+profile = profile ./ max(profile(:))
+
 #[cycle] = zyklendetektion(profile, 0.001)
 [cycle] = zyklendetektion(profile, 0.25)
 
