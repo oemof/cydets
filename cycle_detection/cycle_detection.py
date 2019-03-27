925bfff00146f77dd2ab2ba599ec9b1ed2eced63
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-03-27 17:00:30 +0100

    adjusted docstrings

--- cycle_detection/cycle_detection.py
+++ cycle_detection/cycle_detection.py
@@ -8,8 +8,34 @@ import numpy as np
 import pandas as pd
 
 def detect_cycles(path):
-    """
-    Detect cycles: Read input data and search for cycles.
+    r"""
+    This function detects cycles in a time series with information on start
+    time, end time and depths of cycle:
+
+    - Read time series from supplied :code:`path`.
+    - Normalize input time series c:
+
+    .. math::
+
+        ts_{z}\left(t \right) = ts\left(t \right) - min\left(t \right)
+
+        ts_{norm}\left(t \right) = \frac{ts_{z}\left(t \right)}
+        {max\left(ts_{z} \right)}
+
+        ts \text{: time series}
+
+        ts_{z} \text{: with zero point}
+
+        ts_{norm} \text{: normalised}
+
+    - find peaks (and valley) of the series.
+      (:func:`cycle_detection.find_peaks_valleys_idx`).
+    - find possible starting and ending points for precycles
+      (:func:`cycle_detection.soc_find_start`,
+      :func:`cycle_detection.soc_find_end`).
+    - identify precycles (:func:`cycle_detection.search_precycle`)
+    - identify actual cycles by removing overlapping precycles.
+    - calculate the depths of the actual cycles.
 
     Parameters
     ----------
@@ -18,13 +44,15 @@ def detect_cycles(path):
 
     Returns
     -------
-    cycles : pandas.core.frame.DataFrame
+    df : pandas.core.frame.DataFrame
         Dataframe containing the cycles start times, end times, local minima
-        and amplitude.
+        and depth of cycle (doc).
 
     Note
     ----
-    The DataFrame's collumns are 't_start', 't_end', 't_minimum' and 'amplitude'.
+    The DataFrame's collumns are :code:`t_start` (starttime),
+    :code:`t_end` (endtime), :code:`t_minimum` (time of local minimum in cycle)
+    and :code:`doc` (depths of cycle).
     """
 
     # read input data from .csv file
@@ -34,69 +62,44 @@ def detect_cycles(path):
     series['norm'] = series['values'] - series['values'].min()
     series['norm'] /= series['norm'].max()
 
-    # calculate cycles
-    cycles = find_cycles(series['norm'])
-    print(type(series['norm']))
-    return cycles
-
-def find_cycles(series_norm):
-    """
-    Search the normalised time series for cycles and detect start times, end times,
-    local minimima and amplitudes with the corresponding functions.
-
-    Parameters
-    ----------
-    series_norm : pandas.core.series.Series
-        Normalised input time series.
-
-    Returns
-    -------
-    df : pandas.core.frame.DataFrame
-        Dataframe containing the cycles start times, end times, local minima
-        and amplitude.
-    """
     # find minima and maxima
-    min_idx, max_idx = find_peaks_idx(series_norm)
-    # find start and end times
-    t_start = soc_find_start(series_norm, max_idx)
-    t_end = soc_find_end(series_norm, max_idx)
+    min_idx, max_idx = find_peaks_valleys_idx(series['norm'])
+    # find start and end times of cycles (potenial start/end at local maxima)
+    t_start = soc_find_start(series['norm'], max_idx)
+    t_end = soc_find_end(series['norm'], max_idx)
 
-    # precalculation of the cycles
-    precycles = search_precycle(series_norm, max_idx, t_start, t_end)
-
-    # remove rows with zeros only and remove duplicates
-    precycles_mod = precycles[~np.all(precycles == 0, axis=1)]
-    precycles_mod = np.unique(precycles_mod, axis=0)
+    # search for precycles
+    precycles = search_precycle(series['norm'], max_idx, t_start, t_end)
 
     # cycle detection
-    cycles = cycling(precycles_mod)
+    cycles = cycling(precycles)
 
     # calculate the amplitude of the cycles
-    cycles[:, 3] = calc_amplitude(series_norm, cycles)
+    cycles[:, 3] = calc_doc(series['norm'], cycles)
 
     # write data to DataFrame
     df = pd.DataFrame()
     df['t_start'] = cycles[:, 0]
     df['t_end'] = cycles[:, 1]
     df['t_minimum'] = cycles[:, 2]
-    df['amplitude'] = cycles[:, 3]
+    df['doc'] = cycles[:, 3]
 
     return df
 
 
-def find_peaks_idx(series):
+def find_peaks_valleys_idx(series):
     """
-    Find the indices of local minima and maxima:
+    Find the indices of peaks and valleys.
 
     - Calculate the difference of neighboring values in the series.
     - Search for sign changes in the differences.
     - Remove sign changes for zeros (zero has an own sign in numpy).
     - Iterate over indices of the sign changes:
 
-        - local minimum, if gradient before sign change is negative.
-        - local maximum, if gradient before sign change is positive.
+        - valley, if gradient before sign change is negative.
+        - peak, if gradient before sign change is positive.
 
-    - Remove first local minimum/maximum, if it is the first value of the series.
+    - Remove peak/valley, if it is at the first value of the series.
 
     Parameters
     ----------
@@ -110,6 +113,13 @@ def find_peaks_idx(series):
 
     max_idx : list
         List with the indices of local maxima in the series.
+
+    Note
+    ----
+    A peak is a point, where the value is higher than the value of the
+    preceding timestep and not less than the succeeding value.
+    A valley is a point, where the value is lower than the value of the
+    preceding timestep and not more than the succeeding value.
     """
     # calculate difference between neighboring values
     diff = np.diff(np.concatenate(([0], series, [0])))
@@ -147,7 +157,8 @@ def find_peaks_idx(series):
 
 def soc_find_start(series, indices):
     """
-    Find the starting times of the cycles.
+    Find the starting times of the precycles. A possible starting point for a
+    cycle is a peak that is higher than specific a succeeding peak.
 
     Parameters
     ----------
@@ -164,8 +175,14 @@ def soc_find_start(series, indices):
     """
     t = []
 
+    # number of peaks
     size = len(indices)
 
+    # determine possible starting points for precycles:
+    # searches the peaks from the end to the start for a peak at t1 that is
+    # higher than the last peak at t2
+    # if a peak is found or all preceding peaks have been searched
+    # t2 is moved to its predecessor
     for c in range(1, size)[::-1]:
         for d in range(c)[::-1]:
             if series[indices[d]] >= series[indices[c]]:
@@ -181,7 +198,8 @@ def soc_find_start(series, indices):
 
 def soc_find_end(series, indices):
     """
-    Find the ending times of the cycles.
+    Find the ending times of the precycles. A possible ending point for a
+    cycle is a peak that is higher than specific a preceding peak.
 
     Parameters
     ----------
@@ -198,8 +216,14 @@ def soc_find_end(series, indices):
     """
     t = []
 
+    # number of peaks
     size = len(indices)
 
+    # determine possible ending points for precycles:
+    # searches the peaks from start to end for a peak at t2 that is
+    # higher than the first peak at t1
+    # if a peak is found or all preceding peaks have been searched
+    # t1 is moved to its successor
     for c in range(size):
         for d in range(c + 1, size):
             if series[indices[d]] >= series[indices[c]]:
@@ -215,7 +239,9 @@ def soc_find_end(series, indices):
 
 def search_precycle(series, indices, t_start, t_end):
     """
-    Precalculation of the cycles.
+    Search for precycles:
+    Precycles start at the calculated starting times and end at the next peak
+    or start at a previous peak and end at the calculated ending times.
 
     Parameters
     ----------
@@ -242,29 +268,45 @@ def search_precycle(series, indices, t_start, t_end):
 
     for c in range(size):
         if t_start[c] < indices[c]:
+            # start time must be before time of corresponding peak
+            # retrieve the values of the series between start time and next peak
             values = series[t_start[c]:indices[c] + 1].tolist()
 
+            # start time
             pre_a[c, 0] = t_start[c]
+            # next peak
             pre_a[c, 1] = indices[c]
+            # index/time of minimum value
             pre_a[c, 2] = values.index(min(values)) + t_start[c]
+            # minimum value of the section
             pre_a[c, 3] = min(values)
 
         if t_end[c] > indices[c]:
+            # end time must be after time of corresponding peak
+            # retrieve the values of the series between previous peak and end time
             values = series[indices[c]:t_end[c] + 1].tolist()
 
+            # previous peak
             pre_b[c, 0] = indices[c]
+            # end time
             pre_b[c, 1] = t_end[c]
+            # index/time of minimum value
             pre_b[c, 2] = values.index(min(values)) + indices[c]
+            # minimum value of the section
             pre_b[c, 3] = min(values)
 
     # concatenate the two arrays
     pre = np.append(pre_a, pre_b, axis=0)
+    # remove rows with only zeros (=no precycle found)
+    pre = pre[~np.all(pre == 0, axis=1)]
+    # remove duplicates
+    pre = np.unique(pre, axis=0)
     return pre
 
 
 def cycling(rows):
     """
-    Cycle detection, delete from precycles if ...?
+    Cycle detection from given precycles.
 
     Parameters
     ----------
@@ -276,41 +318,47 @@ def cycling(rows):
     rows : numpy.ndarray
         Array of cycles.
     """
-    # remove rows
+    # remove overlaps in the precycles and flag the corresponding rows
     for c in range(rows.shape[0]):
         indices = np.where((rows[c, 2] == rows[:, 2]) &
                            (rows[c, 0] <= rows[:, 0]) &
                            (rows[c, 1] >= rows[:, 1]))[0]
+        # choose indices to flag rows
         indices = indices[indices != c]
         rows[indices] = 0
 
-    # remove rows with zeros only
+    # remove the flagged rows (rows with zeros only)
     rows = rows[~np.all(rows == 0, axis=1)]
 
     return rows
 
 
-def calc_amplitude(series, pre):
-    """
-    Calculation of the cycles amplitude.
+def calc_doc(series, rows):
+    r"""
+    Calculation of the depths of cycle.
+
+    .. math::
+
+        doc\left(c \right)= min\left[soc\left(t_{start}\right),
+        soc\left(t_{end}\right) \right] - c_{min}
 
     Parameters
     ----------
     series : pandas.core.series.Series
         Input series.
 
-    pre : numpy.ndarray
-        Array of precycles.
+    rows : numpy.ndarray
+        Array of cycles.
 
     Returns
     -------
-    amp : numpy.ndarray
-        Array of corresponding amplitudes.
+    doc : numpy.ndarray
+        Array of corresponding depth of the cylces.
     """
-    num = pre.shape[0]
-    amp = np.zeros(num)
+    num = rows.shape[0]
+    doc = np.zeros(num)
 
     for c in range(num):
-        amp[c] = min(series[pre[c, 0]], series[pre[c, 1]]) - pre[c, 3]
+        doc[c] = min(series[rows[c, 0]], series[rows[c, 1]]) - rows[c, 3]
 
-    return amp
+    return doc
