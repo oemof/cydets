03b350aa74fde5dac89e948a5fb10a4ccff82baf
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-03-28 13:39:46 +0100

    minor modifications to setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ def read(fname):
 
 setup(name='cycle detection',
       version='0.0.1',
-      description='Cycle detetction software. Detect cycle amplitudes and frequency in time series.',
+      description=(
+              'Cycle detetction software. Detect detphs of cycle and cycle '
+              'frequency in time series.'),
       url='https://github.com/oemof/cycle_detect',
       author='Cord Kaldemeyer, Francesco Witte',
       author_email='francesco.witte@hs-flensburg.de',
